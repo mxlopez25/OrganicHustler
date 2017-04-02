3fd13402d084d57c6309e561bd4a37bd50fc8f66
hectorandac01 <hectorcuriso4@gmail.com>
2017-04-02 09:32:49 -0400

    Better looks

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,24 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
+Rails.application.config.assets.precompile += %w( bootstrap.css )
+Rails.application.config.assets.precompile += %w( jquery.min.js )
+Rails.application.config.assets.precompile += %w( admin.js )
+Rails.application.config.assets.precompile += %w( admin_layout.css )
+Rails.application.config.assets.precompile += %w( dropzone.js )
+Rails.application.config.assets.precompile += %w( dropzone.css )
+Rails.application.config.assets.precompile += %w( bootstrap-tagsinput.js )
+Rails.application.config.assets.precompile += %w( bootstrap-tagsinput.css )
+Rails.application.config.assets.precompile += %w( style-fc.css )
+Rails.application.config.assets.precompile += %w( dragable.min.js )
+Rails.application.config.assets.precompile += %w( jquery.jcarousel.js )
+Rails.application.config.assets.precompile += %w( magnific-popup/magnific-popup.css )
+Rails.application.config.assets.precompile += %w( bootstrap.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( jscolor/jscolor.js )
+Rails.application.config.assets.precompile += %w( jscolor/jscolor.min.js )
+Rails.application.config.assets.precompile += %w( index/estilos.css )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
