62a7ee8cfaca3f55c1194ffe982064dcfad9b3c0
hectorandac01 <hectorcuriso4@gmail.com>
2017-05-13 22:27:40 -0400

    Styles

--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -338,7 +338,6 @@ if (typeof jQuery === 'undefined') {
     Carousel.TRANSITION_DURATION = 600
 
     Carousel.DEFAULTS = {
-    interval: 5000,
     pause: 'hover',
         wrap: true,
         keyboard: true
