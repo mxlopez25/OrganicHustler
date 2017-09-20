4d525818d105a0b303ed072edd28f6c255027eb6
hectorandac01 <hectorcuriso4@gmail.com>
2017-09-20 16:41:42 -0400

    Fixed counter

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -51,10 +51,14 @@ class HomeController < ApplicationController
   def get_items
     products_cat = Category.find_by_title params[:category]
     products_sty = Style.find_by_id params[:style]
+    products_col = params[:color].blank? ? nil : params[:color]
+    products_mat = Material.find_by_id params[:material]
     sql = "SELECT products.* FROM products
           INNER JOIN categories_products ON products.id = categories_products.product_id
           #{products_sty ? 'INNER JOIN products_styles ON products.id = products_styles.product_id' : ''}
-          WHERE category_id = '#{products_cat.id}' #{products_sty ? ' AND style_id = ' + products_sty.id.to_s : ''}"
+    #{products_col ? 'INNER JOIN colors ON products.id = colors.product_id' : ''}
+    #{products_mat ? 'INNER JOIN materials_products ON products.id = materials_products.product_id' : ''}
+          WHERE category_id = '#{products_cat.id}' #{products_sty ? ' AND style_id = ' + products_sty.id.to_s : ''} #{products_col ? 'AND colors.title = \'' + products_col + '\'' : ''} #{products_mat ? ' AND material_id = ' + products_mat.id.to_s : ''}"
 
 
     results = ActiveRecord::Base.connection.execute(sql)
