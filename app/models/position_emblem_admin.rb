a5cbb5a85177c85bd1b673b029ce5d4ba9c1ccde
hectorandac01 <hectorcuriso4@gmail.com>
2017-09-27 15:59:43 -0400

    Created display position on admin page edit

--- app/models/position_emblem_admin.rb
+++ app/models/position_emblem_admin.rb
@@ -2,5 +2,8 @@ class PositionEmblemAdmin < ApplicationRecord
   has_attached_file :picture, styles: {medium: "300x300>", thumb: "100x100>"}, default_url: "/images/no-logo.jpg"
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
 
+  def color_image
+    (Color.find self.color_id).product_images.where(main: true).first
+  end
 
 end
