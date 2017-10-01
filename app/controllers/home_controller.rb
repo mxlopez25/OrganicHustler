46731dedc19b33f2478050ce7f4ab2504b8332af
hectorandac01 <hectorcuriso4@gmail.com>
2017-10-01 13:55:08 -0400

    Fixed search bar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -56,7 +56,7 @@ class HomeController < ApplicationController
           #{products_sty ? 'INNER JOIN products_styles ON products.id = products_styles.product_id' : ''}
     #{products_col ? 'INNER JOIN colors ON products.id = colors.product_id' : ''}
     #{products_mat ? 'INNER JOIN materials_products ON products.id = materials_products.product_id' : ''}
-          WHERE #{products_cat ? ' category_id = '+ products_cat.id.to_s : ' products.title LIKE \'%'+ (params[:search].to_s) +'%\'' } #{products_sty ? ' AND style_id = ' + products_sty.id.to_s : ''} #{products_col ? 'AND colors.title = \'' + products_col + '\'' : ''} #{products_mat ? ' AND material_id = ' + products_mat.id.to_s : ''}"
+          WHERE #{products_cat ? ' category_id = '+ products_cat.id.to_s : params[:search].blank? ? '' : ' products.title LIKE \'%'+ (params[:search].to_s) +'%\'' } #{products_sty ? ' AND style_id = ' + products_sty.id.to_s : ''} #{products_col ? 'AND colors.title = \'' + products_col + '\'' : ''} #{products_mat ? ' AND material_id = ' + products_mat.id.to_s : ''}"
 
     p sql
 
