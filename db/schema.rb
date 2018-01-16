e743cc0d1d9e21cff37a05b7766e15e50b95eff4
Hector Acosta <hectorcuriso4@gmail.com>
2018-01-16 06:17:01 +0000

    YML23

--- db/schema.rb
+++ db/schema.rb
@@ -216,18 +216,17 @@ ActiveRecord::Schema.define(version: 20180115063549) do
   end
 
   create_table "orders", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
-    t.string   "state",                                                        collation: "utf8_general_ci"
-    t.text     "description",       limit: 65535,                              collation: "utf8_general_ci"
-    t.string   "carrier",                                                      collation: "utf8_general_ci"
-    t.string   "tracking_code",                                                collation: "utf8_general_ci"
-    t.string   "overall_user_id",                                              collation: "utf8_general_ci"
-    t.string   "overall_user_type",                                            collation: "utf8_general_ci"
-    t.string   "charge_id",                                                    collation: "utf8_general_ci"
-    t.boolean  "confirmed",                       default: false
-    t.string   "tag_link",                                                     collation: "utf8_general_ci"
-    t.string   "user_address_id",                                              collation: "utf8_general_ci"
-    t.datetime "updated_at",                                      null: false
-    t.datetime "created_at",                                      null: false
+    t.string   "state",                                        collation: "utf8_general_ci"
+    t.text     "description",       limit: 65535,              collation: "utf8_general_ci"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.string   "carrier",                                      collation: "utf8_general_ci"
+    t.string   "tracking_code",                                collation: "utf8_general_ci"
+    t.string   "overall_user_id",                              collation: "utf8_general_ci"
+    t.string   "overall_user_type",                            collation: "utf8_general_ci"
+    t.string   "charge_id",                                    collation: "utf8_general_ci"
+    t.string   "tag_link",                                     collation: "utf8_general_ci"
+    t.string   "user_address_id",                              collation: "utf8_general_ci"
   end
 
   create_table "pictures", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
