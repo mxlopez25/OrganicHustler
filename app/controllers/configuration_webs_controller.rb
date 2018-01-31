4aa1153ca21b54bfe15500636d2c0d05a120a133
hectorandac01 <hectorcuriso4@gmail.com>
2018-01-31 00:29:48 -0800

    YML2

--- app/controllers/configuration_webs_controller.rb
+++ app/controllers/configuration_webs_controller.rb
@@ -1,9 +1,17 @@
 class ConfigurationWebsController < ApplicationController
 
   def update
-    co = ConfigurationWeb.find(params[:id])
-    co.value = params[:value]
-    co. save!
+    co = ConfigurationWeb.find(params[:id] ||  params[:configuration_web][:id])
+    if params[:value]
+      co.value = params[:value]
+    end
+
+    if params[:configuration_web]
+      co.picture = params[:configuration_web][:picture]
+    end
+
+    co.save!
+    redirect_to '/admin/home'
   end
 
 end
