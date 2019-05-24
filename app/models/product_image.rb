bab356ed9c2660463afe04a06b2c416ea0fa7e52
unknown <7888441+mxlopez25@users.noreply.github.com>
2019-05-24 14:49:33 -0400

    Image conversion Rearrenge

--- app/models/product_image.rb
+++ app/models/product_image.rb
@@ -1,16 +1,16 @@
 class ProductImage < ApplicationRecord
   has_attached_file :picture, 
                     styles: { 
-                      thumb: ["120x120>", :webp], 
-                      small: ["300x300>", :webp], 
-                      med: ["500x500>", :webp], 
-                      large: ["900x900>", :webp] 
+                      large: "900x900>", 
+                      med: "500x500>", 
+                      small: "300x300>", 
+                      thumb: "120x120>" 
                     },
                     convert_options: {
-                      thumb: '-quality 85',
-                      small: '-quality 85',
+                      large: '-quality 85',
                       med: '-quality 85',
-                      large: '-quality 85'
+                      small: '-quality 85',
+                      thumb: '-quality 85'
                   }
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
   belongs_to :color
