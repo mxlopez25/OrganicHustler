76367808250219fc08fcf699c2f00646440f6910
unknown <7888441+mxlopez25@users.noreply.github.com>
2019-05-24 14:16:43 -0400

    Update on Image Conversion, Huge size added

--- app/models/product_image.rb
+++ app/models/product_image.rb
@@ -5,14 +5,16 @@ class ProductImage < ApplicationRecord
                       small: ["300x300>", :webp], 
                       med: ["500x500>", :webp], 
                       large: ["900x900>", :webp], 
-                      big: ["1080x1080>", :webp] 
+                      big: ["1080x1080>", :webp], 
+                      huge: ["1980x1980>", :webp] 
                     },
                     convert_options: {
                       thumb: '-quality 85',
                       small: '-quality 85',
                       med: '-quality 85',
                       large: '-quality 85',
-                      big: '-quality 85'
+                      big: '-quality 85',
+                      huge: '-quality 85'
                   }
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
   belongs_to :color
