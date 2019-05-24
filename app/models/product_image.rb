f23d8ba1efbf972048b5e5f90aa4506c1304ab82
unknown <7888441+mxlopez25@users.noreply.github.com>
2019-05-24 13:52:06 -0400

    Update on Image Conversion Attachement

--- app/models/product_image.rb
+++ app/models/product_image.rb
@@ -5,7 +5,13 @@ class ProductImage < ApplicationRecord
                       small: "100x100#", 
                       med: "150x150>", 
                       large: "200x200>" 
-                    }
+                    },
+                    convert_options: {
+                      thumb: '-quality 85',
+                      small: '-quality 85',
+                      med: '-quality 85',
+                      large: '-quality 85'
+                  }
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
   belongs_to :color
 end
