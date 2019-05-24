7607e348eb862c54277022398a7e4effd2e7a7d8
unknown <7888441+mxlopez25@users.noreply.github.com>
2019-05-24 15:23:07 -0400

    Image Conversion

--- app/models/product_image.rb
+++ app/models/product_image.rb
@@ -1,18 +1,29 @@
 class ProductImage < ApplicationRecord
   has_attached_file :picture, 
-  styles: { 
-                      thumb: "64x64#", 
-                      small: "100x100#", 
-                      med: "150x150>", 
-                      large: "200x200>" 
-                    
-  },
-  convert_options: {
-                      thumb: '-quality 85',
-                      small: '-quality 85',
-                      med: '-quality 85',
-                      large: '-quality 85'
-                  }
+                    styles: {
+                      big_safari: ['1080x1080>', :jp2],
+                      big_chrome: ['1080x1080>', :webp],
+                      large_safari: ['900x900>', :jp2],
+                      large_chrome: ['900x900>', :webp],
+                      medium_safari: ['500x500>', :jp2],
+                      medium_chrome: ['500x500>', :webp],
+                      thumb_safari: ['300x300>', :jp2],
+                      thumb_chrome: ['300x300>', :webp],
+                      s_thumb_safari: ['120x120>', :jp2],
+                      s_thumb_chrome: ['120x120>', :webp]
+                    },
+                    convert_options: {
+                      big_safari: '-quality 85',
+                      big_chrome: '-quality 85',
+                      large_safari: '-quality 85',
+                      large_chrome: '-quality 85',
+                      medium_safari: '-quality 85',
+                      medium_chrome: '-quality 85',
+                      thumb_safari: '-quality 85',
+                      thumb_chrome: '-quality 85',
+                      s_thumb_safari: '-quality 85',
+                      s_thumb_chrome: '-quality 85'
+                    }
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
   belongs_to :color
 end
