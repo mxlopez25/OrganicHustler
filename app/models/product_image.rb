f32ab3b7bca4d5d37196bf5048475b1875ee2980
unknown <7888441+mxlopez25@users.noreply.github.com>
2019-05-24 14:22:49 -0400

    Change Conversion Order

--- app/models/product_image.rb
+++ app/models/product_image.rb
@@ -1,20 +1,20 @@
 class ProductImage < ApplicationRecord
   has_attached_file :picture, 
                     styles: { 
-                      thumb: ["120x120>", :webp], 
-                      small: ["300x300>", :webp], 
-                      med: ["500x500>", :webp], 
+                      huge: ["1980x1980>", :webp],
+                      big: ["1080x1080>", :webp],
                       large: ["900x900>", :webp], 
-                      big: ["1080x1080>", :webp], 
-                      huge: ["1980x1980>", :webp] 
+                      med: ["500x500>", :webp], 
+                      small: ["300x300>", :webp], 
+                      thumb: ["120x120>", :webp]
                     },
                     convert_options: {
-                      thumb: '-quality 85',
-                      small: '-quality 85',
-                      med: '-quality 85',
-                      large: '-quality 85',
-                      big: '-quality 85',
                       huge: '-quality 85'
+                      med: '-quality 85',
+                      big: '-quality 85',
+                      large: '-quality 85',
+                      small: '-quality 85',
+                      thumb: '-quality 85'
                   }
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
   belongs_to :color
