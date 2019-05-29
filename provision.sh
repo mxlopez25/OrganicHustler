16c9602c4071630c29146d212cbf82445fcde9b3
unknown <7888441+mxlopez25@users.noreply.github.com>
2019-05-29 01:03:01 -0400

    Cleaning of provision file

--- provision.sh
+++ provision.sh
@@ -1,13 +1,11 @@
 sudo apt-get update -y --fix-missing
 
-sudo apt-get install libmysqlclient-dev
-
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
 sudo apt-get update -y
-sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn
+sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn imagemagick libmagickcore-dev libmagickwand-dev
 
 cd
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
@@ -22,18 +20,10 @@ exec $SHELL
 rbenv install 2.5.0
 rbenv global 2.5.0
 ruby -v
-
 cd /vagrant
-
 gem install bundler -v 1.16.2
-
 rbenv rehash
-
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt-get install -y nodejs
-
 gem install rails -v 5.0.1
-
 rbenv rehash
 
 #sudo apt-get -y install mysql-server mysql-client libmysqlclient-dev
