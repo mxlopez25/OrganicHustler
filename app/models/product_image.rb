61c8d53934c9c2108620aacbfe0ca1f100667cdf
unknown <7888441+mxlopez25@users.noreply.github.com>
2019-05-29 01:02:39 -0400

    Update on Product Image

--- app/models/product_image.rb
+++ app/models/product_image.rb
@@ -1,16 +1,28 @@
 class ProductImage < ApplicationRecord
   has_attached_file :picture, 
-                    styles: { 
-                      large: "900x900>", 
-                      med: "500x500>", 
-                      small: "300x300>", 
-                      thumb: "120x120>" 
+                    styles: {
+                      big_safari: '1080x1080>',
+                      big_chrome: '1080x1080>',
+                      large_safari: '900x900>',
+                      large_chrome: '900x900>',
+                      medium_safari: '500x500>',
+                      medium_chrome: '500x500>',
+                      thumb_safari: '300x300>',
+                      thumb_chrome: '300x300>',
+                      s_thumb_safari: '120x120>',
+                      s_thumb_chrome: '120x120>'
                     },
                     convert_options: {
-                      large: '-quality 85',
-                      med: '-quality 85',
-                      small: '-quality 85',
-                      thumb: '-quality 85'
+                      big_safari: '-quality 85',
+                      big_chrome: '-quality 85',
+                      large_safari: '-quality 85',
+                      large_chrome: '-quality 85',
+                      medium_safari: '-quality 85',
+                      medium_chrome: '-quality 85',
+                      thumb_safari: '-quality 85',
+                      thumb_chrome: '-quality 85',
+                      s_thumb_safari: '-quality 85',
+                      s_thumb_chrome: '-quality 85'
                   }
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
   belongs_to :color
